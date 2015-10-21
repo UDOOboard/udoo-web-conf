cb29ff820627b886c68db3a57ba1f4b62607ecfe
UDOOBoard <social@udoo.org>
2015-10-21 09:38:52 +0000

    Route to docs

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/docs', express.static('docs'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
