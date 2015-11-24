1b8a6df7e53e26b06660b340314d7a5234ada26d
mikelangeloz <mikelangeloz@gmail.com>
2015-11-24 20:56:33 +0900

    proper error handling

--- mysketch/mysketch.ino
+++ mysketch/mysketch.ino
@@ -1,7 +1,14 @@
-void setup() { 
-  pinMode(13, OUTPUT);
+void setup() {
+  // put your setup code here, to run once:
+
+
 }
+
+
 void loop() {
-  digitalWrite(13, HIGH);
+  // put your main code here, to run repeatedly:
+
+
 }
-​
+
+
