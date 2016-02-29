e1501556df5ccc11b807c30a09503c4676d55cf1
Francesco Montefoschi <francesco.monte@gmail.com>
2016-03-01 01:41:21 +0900

    Rendering optimizations

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var oneDay = 86400000;
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneDay }));
 
 app.use('/', routes);
 app.use('/settings', settingsRoutes);
