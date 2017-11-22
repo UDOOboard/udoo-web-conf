512ded0ea054a476efed766e4bec85cb29fe7217
Francesco Montefoschi <francesco.monte@gmail.com>
2017-11-22 12:08:25 +0100

    Low priority updates

--- bin/apt-update.sh
+++ bin/apt-update.sh
@@ -4,5 +4,5 @@ PID=`ps aux | grep "[a]pt update" |awk '{print $2}'`
 
 if [ -z "$PID" ]; then
     echo "Starting APT Update"
-    DEBIAN_FRONTEND=noninteractive apt update && sync &
+    DEBIAN_FRONTEND=noninteractive nice -19 apt update && sync &
 fi
